8b195d4e51df8df400284fe88ae04776b63caaee
James Golick <jamesgolick@gmail.com>
2012-12-07 13:42:40 -0800

    start up app

--- src/memcached_app.erl
+++ src/memcached_app.erl
@@ -2,8 +2,16 @@
 
 -behaviour(application).
 
+-export([start/0]).
+
 %% Application callbacks
 -export([start/2, stop/1]).
+
+start() ->
+  application:start(compiler),
+  application:start(syntax_tools),
+  application:start(lager),
+  application:start(memcached).
 
 %% ===================================================================
 %% Application callbacks
