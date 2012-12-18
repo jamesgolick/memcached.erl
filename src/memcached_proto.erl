06a12d18a0e9236a9b7f619f9b27aa5d8f1acf9e
James Golick <jamesgolick@gmail.com>
2012-12-17 23:08:21 -0800

    frame_packets -> frame

--- src/memcached_proto.erl
+++ src/memcached_proto.erl
@@ -1,6 +1,6 @@
 -module(memcached_proto).
 
--export([frame_packets/1]).
+-export([frame/1]).
 
 -define(MAGIC_RESPONSE, 16#81).
 
@@ -8,10 +8,10 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
-frame_packets(Packets) ->
-  frame_packets(Packets, []).
+frame(Packets) ->
+  frame(Packets, []).
 
-frame_packets(Packets, Framed) ->
+frame(Packets, Framed) ->
   case size(Packets) of
     Size when Size >= 24 ->
       <<Header:24/binary, _/binary>> = Packets,
@@ -24,7 +24,7 @@ frame_packets(Packets, Framed) ->
 	  {incomplete, Packets, lists:reverse(Framed)};
 	RemainderSize when RemainderSize > BodyLength ->
 	  <<Body:BodyLength/binary, Remainder/binary>> = BodyAndRemainder,
-	  frame_packets(Remainder, [<<Header/binary,Body/binary>> | Framed]);
+	  frame(Remainder, [<<Header/binary,Body/binary>> | Framed]);
 	BodyLength ->
 	  AllFramed = [<<Header/binary,BodyAndRemainder/binary>> | Framed],
 	  {complete, lists:reverse(AllFramed)}
@@ -33,28 +33,29 @@ frame_packets(Packets, Framed) ->
       {incomplete, Packets, Framed}
   end.
 
+
 -ifdef(TEST).
 
-frame_packets_test() ->
+frame_test() ->
   Message = <<129,0,0,0,4,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,10,222,173,190,239,98,115,100,102>>,
-  ?assertEqual({complete, [Message]}, frame_packets(Message)).
+  ?assertEqual({complete, [Message]}, frame(Message)).
 
-incomplete_header_frame_packets_test() ->
+incomplete_header_frame_test() ->
   FirstPacket = <<129,0,0,0,4,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,10,222,173,190,239,98,115,100,102>>,
   Message = <<FirstPacket/binary, 129>>,
-  ?assertEqual({incomplete, <<129>>, [FirstPacket]}, frame_packets(Message)).
+  ?assertEqual({incomplete, <<129>>, [FirstPacket]}, frame(Message)).
 
-incomplete_body_frame_packets_test() ->
+incomplete_body_frame_test() ->
   FirstPacket = <<129,0,0,0,4,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,10,222,173,190,239,98,115,100,102>>,
   Remainder = <<129,0,0,0,4,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,10>>,
   Message = <<FirstPacket/binary, Remainder/binary>>,
-  ?assertEqual({incomplete, Remainder, [FirstPacket]}, frame_packets(Message)).
+  ?assertEqual({incomplete, Remainder, [FirstPacket]}, frame(Message)).
 
-multiple_packets_frame_packets_test() ->
+multiple_packets_frame_test() ->
   FirstPacket = <<129,0,0,0,4,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,10,222,173,190,239,98,115,100,102>>,
   SecondPacket = <<129,0,0,0,4,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,10,222,173,190,239,98,115,100,102>>,
   Message = <<FirstPacket/binary, SecondPacket/binary>>,
-  Result = frame_packets(Message),
+  Result = frame(Message),
   ?assertEqual({complete, [FirstPacket, SecondPacket]}, Result).
 
 -endif.
