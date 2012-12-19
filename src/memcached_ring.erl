b4e7d036760f6bd2d4f3691ca05b2a03aecc3ac5
James Golick <jamesgolick@gmail.com>
2012-12-18 22:19:34 -0800

    try rewriting the ring to use gb_trees

--- src/memcached_ring.erl
+++ src/memcached_ring.erl
@@ -7,16 +7,61 @@
 
 -define(RING_SIZE, 1000).
 
-create([Server | Servers]) ->
-  create(Servers, memcached_chash:fresh(?RING_SIZE, Server)).
+create(Servers) ->
+  create(Servers, gb_trees:empty()).
 
 create([], Ring) ->
   Ring;
 create([Server | Servers], Ring) ->
-  OtherRing = memcached_chash:fresh(?RING_SIZE, Server),
-  create(Servers, memcached_chash:merge_rings(Ring, OtherRing)).
+  ServerBinary = list_to_binary(Server),
+  NewRing = lists:foldl(fun(I, InnerRing) ->
+	IString = list_to_binary(integer_to_list(I)),
+	Key = <<ServerBinary/binary, "-", IString/binary>>,
+	<<Hash1:40/integer, Hash2:40/integer,
+	  Hash3:40/integer, Hash4:40/integer>> = crypto:sha(Key),
+	gb_trees:enter(Hash1, Server,
+	  gb_trees:enter(Hash2, Server,
+	    gb_trees:enter(Hash3, Server,
+	      gb_trees:enter(Hash4, Server, InnerRing))))
+      end, Ring, lists:seq(0, 39)),
+  create(Servers, NewRing).
 
-get(Key, Ring) ->
-  KeyHash = memcached_chash:key_of(Key),
-  [{_Hash, Node}] = memcached_chash:successors(KeyHash, Ring, 1),
+get(Key, {_, Tree}) ->
+  <<Hash:40/integer, _/binary>> = crypto:sha(Key),
+  {_, Node} = find_nearest(Hash, Tree, {0, 0}),
   Node.
+
+find_nearest(Hash, {Key, Value, Smaller, Bigger}, {RK, RV}) ->
+  Result2 = case Key of
+    Key when RK == 0 ->
+      {Key, Value};
+    Key when Key > Hash andalso Key < RK ->
+      {Key, Value};
+    _ ->
+      {RK, RV}
+  end,
+  {RK2, RV2} = Result2,
+  {SK, SV} = case Smaller of
+    nil ->
+      Result2;
+    {SmallerKey, _, _, _} when SmallerKey > RK2 ->
+      Result2;
+    _ ->
+      find_nearest(Hash, Smaller, Result2)
+  end,
+  {BK, BV} = case Bigger of
+    nil ->
+      Result2;
+    {BiggerKey, _, _, _} when BiggerKey > RK2 ->
+      Result2;
+    _ ->
+      find_nearest(Hash, Bigger, Result2)
+  end,
+  case SK of
+    SK when SK > Key ->
+      {Key, Value};
+    SK when SK < BK ->
+      {SK, SV};
+    _ ->
+      {BK, BV}
+  end.
