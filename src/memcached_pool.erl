4127e050bdc1905ac39e0a2a7718cbf5aa074d6f
James Golick <jamesgolick@gmail.com>
2013-08-23 18:41:59 -0700

    make sure that if we take a connection out of the pool, it's ready to receive requests

--- src/memcached_pool.erl
+++ src/memcached_pool.erl
@@ -7,4 +7,14 @@ create(Server) ->
   memcached_sup:add_server(Server).
 
 with_connection(Server, Fun) ->
-  poolboy:transaction(list_to_atom(Server), Fun).
+  poolboy:transaction(list_to_atom(Server), fun(Conn) ->
+	{status, _, _, [_,_,_,_,[_,{data,[_,_,_,{"StateName", State}]},_]]} = sys:get_status(Conn),
+	case State of
+	  ready ->
+	    Fun(Conn);
+	  SomethingElse ->
+	    lager:error("Checked out a connection with state ~p.", SomethingElse),
+	    exit(Conn, normal),
+	    with_connection(Server, Fun)
+	end
+    end).
